57d408ea89487699cfe8c85ac6b7d7e470b3ec5e
Kristopher Micinski <krismicinski@gmail.com>
2018-05-26 21:45:58 -0400

    Broken version of apply rule

--- src/racets-mlang.rkt
+++ src/racets-mlang.rkt
@@ -1,15 +1,23 @@
 #lang racket
 
-(require "facets.rkt")
+(require "facets.rkt" (for-syntax "facets.rkt"))
+(require (for-syntax racket/syntax))
+(require (for-syntax racket/stxparam))
+(require (for-syntax racket/set))
+(require racket/stxparam)
 
 (provide (except-out
           (all-from-out racket)
           lambda
+          with-continuation-mark
+          #%module-begin
           #%app)
-         facet)
+         fac)
 
 (provide (rename-out [fac-lambda lambda]
-                     [fac-app #%app]))
+                     [fac-module-begin #%module-begin]
+                     [fac-app #%app]
+                     [fac-continuation-mark with-continuation-mark]))
 
 (define-syntax-rule (fac prin v1 v2)
   (facet prin v1 v2))
@@ -18,13 +26,31 @@
 (define-syntax-rule (fac-lambda (x ...) expr)
   (lambda (x ...) expr))
 
-; Applications are rewritten to:
-; - 
+(define-syntax (fac-module-begin stx)
+  (syntax-case stx ()
+      [(#%module-begin body ...)
+       #`(#%plain-module-begin
+          (define-syntax-parameter pc (set))
+          body ...)]))
+
+(define-syntax-parameter pc 
+ (lambda (stx)
+      (raise-syntax-error (syntax-e stx) "can only be used inside aif")))
+
 (define-syntax (fac-app stx)
   (syntax-case stx ()
     [(#%app f a0 ...)
      #`(if (facet? f)
-           (facet (facet-label f)
-                  ((facet-left f) a0 ...)
-                  ((facet-right f) a0 ...))
+           (let* ([left
+                   (syntax-parameterize ([pc (set-add #'pc (pos (facet-label f)))])
+                     ((facet-left f) a0 ...))]
+                  [right 
+                   (syntax-parameterize ([pc (set-add #'pc (neg (facet-label f)))])
+                     ((facet-left f) a0 ...))])
+                  (#'facet (facet-label f)
+                   left-side
+                   right-side))
            (f a0 ...))]))
+
+(define-syntax (fac-continuation-mark stx)
+  (wrong-syntax stx "continuation marks are currently unsupported in racets"))
--- src/tests.rkt
+++ src/tests.rkt
@@ -1,10 +1,13 @@
 #lang reader "racets.rkt"
 
 (require "facets.rkt")
+(require racket/stxparam)
 
-(module+ test
-  (require rackunit)
-  (require rackunit/text-ui)
-  (check-eq?
-   ((facet 'bob (lambda (x) x) (lambda (x) 1)) 3)
-   (facet 'bob 3 1)))
+((facet 'bob (lambda (x) x) (lambda (x) 1)) 3)
+
+;(module+ test
+;  (require rackunit)
+;  (require rackunit/text-ui)
+;  (check-equal?
+;   ((facet 'bob (lambda (x) x) (lambda (x) 1)) 3)
+;   (facet 'bob 3 1)))
