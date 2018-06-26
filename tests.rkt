1092ab29631e994daae7c865ee0a5124499109a0
Zhanpeng Wang <zwang10@haverford.edu>
2018-06-26 12:38:54 -0400

    additional test cases for the built-ins. bug not fixed yet.

--- tests.rkt
+++ tests.rkt
@@ -234,6 +234,92 @@
              (fac con2
                   (fac con3 4 5)
                   (fac con3 3 4)))))
+
+  ; Test cases for built-in *
+  (check-equal?
+   (* (fac con0 4 5) 2)
+   (fac con0 8 10))
+  
+  (check-equal?
+   (* (fac con0 2 3) (fac con0 4 5))
+   (fac con0 8 15))
+
+  (check-equal?
+   (* (fac con0 4 5) (fac con1 6 7))
+   (fac con0 (fac con1 24 28) (fac con1 30 35)))
+
+  ; !!!Need to verify that this test case is correct!!!
+  (check-equal?
+   (* (fac con1 (fac con2 2 3) (fac con3 4 5)) (fac con0 (fac con1 6 7) (fac con2 8 9)))
+   (fac con0
+        (fac con1
+             (fac con2 12 18)
+             (fac con3 28 35))
+        (fac con1
+             (fac con2 16 27)
+             (fac con2 (fac con3 32 40) (fac con3 36 45)))))
+
+  (check-equal?
+   (* (fac con0 (fac con1 3 4) (fac con2 5 6)) (fac con3 (fac con1 10 11) (fac con2 7 8)))
+   (fac con0
+        (fac con3
+             (fac con1 30 44)
+             (fac con1
+                  (fac con2 21 24)
+                  (fac con2 28 32)))
+        (fac con3
+             (fac con2
+                  (fac con1 50 55)
+                  (fac con1 60 66))
+             (fac con2 35 48))))
+
+  ; a complicated test case that involves *
+  (check-equal?
+   (* (fac con0 (fac con1 1 2) (fac con2 3 4))
+      (* (fac con1 (fac con2 5 6) (fac con0 7 8))
+         (* (fac con2 9 10) (fac con3 11 12))))
+   (fac con0
+        (fac con1
+             (fac con2
+                  (fac con3 495 540)
+                  (fac con3 660 720))
+             (fac con2
+                  (fac con3 1386 1512)
+                  (fac con3 1540 1680)))
+        (fac con1
+             (fac con2
+                  (fac con3 1485 1620)
+                  (fac con3 2640 2880))
+             (fac con2
+                  (fac con3 2376 2592)
+                  (fac con3 3520 3840)))))
+  
+  ; Test cases for built-in /
+  (check-equal?
+   (/ (fac con0 10 6) 2)
+   (fac con0 5 3))
+
+  (check-equal?
+   (/ (fac con1 8 10) (fac con1 4 5))
+   (fac con1 2 2))
+
+  (check-equal?
+   (/ (fac con1 20 40) (fac con2 4 5))
+   (fac con1 (fac con2 5 4) (fac con2 10 8)))
+
+  (check-equal?
+   (/ (fac con1 (fac con2 100 120) (fac con3 120 60)) (fac con0 (fac con2 2 5) (fac con3 3 4)))
+   (fac con1
+        (fac con0
+             (fac con2 50 24)
+             (fac con2
+                  (fac con3 33 1/3 25)
+                  (fac con3 40 30)))
+        (fac con0
+             (fac con3
+                  (fac con2 60 24)
+                  (fac con2 30 12))
+             (fac con3 40 15))))
   
   ; Test case for the built-in and
   ; it fails for now.
@@ -249,7 +335,8 @@
   (check-equal?
    (or (fac con2 (fac con1 1 2) (fac con1 #t #f)))
    (fac con2 (fac con1 1 2) (fac con1 #t #f)))
-  
+
+  ; it fails for now.
   (check-equal?
    (or (fac con2 #t #f) (fac con1 #t #f))
    (fac con2 (fac con1 #t #t) (fac con1 #t #f)))
@@ -347,7 +434,14 @@
    (fac con2 4 8) "It should return (fac con2 4 8)")
 
   ; Should have a complicated test case that involves built-in, ref-set!, obs, and if
-
+  (check-equal?
+   (let ([x (ref 0)])
+     (begin (if (fac con1 #t #f)
+                (ref-set! x (+ (fac con0 2 0) (fac con1 1 0)))
+                (ref-set! x (- (fac con0 4 5) (fac con1 3 2)))))
+     (deref x))
+   (fac con0 (fac con1 3 2) (fac con1 1 3)))
+  
   ; A more ambitious test of applying builtins to faceted values
   (check-equal? 
    (-
@@ -369,28 +463,21 @@
                 (fac con2 (begin (inc) 1) (begin (inc) 2))
                 (fac con3 (begin (inc) 3) (begin (inc) 4))))
     (fac con3 (begin (inc) 2) (begin (inc) 1)))
-   (fac
-    con1
-    (fac
-     con2
-     (fac con1 (fac con3 3 2) 3)
-     (fac
-      con1
-      (fac
-       con2
-       (fac con1 (fac con3 -1 0) (fac con3 0 1))
-       (fac con3 0 1))
-      3))
-    (fac
-     con2
-     (fac con1 (fac con3 2 1) (fac con3 6 4))
-     (fac
-      con1
-      (fac
-       con2
-       (fac con3 '(1 2) '(1 1))
-       (fac con3 '(2 2) '(2 1)))
-      (fac con3 '(3 2) '(4 1))))))
+   (fac con1
+        (fac con2
+             (fac con1 (fac con3 3 2) 3)
+             (fac con1
+                  (fac con2
+                       (fac con1 (fac con3 -1 0) (fac con3 0 1))
+                       (fac con3 0 1))
+                  3))
+        (fac con2
+             (fac con1 (fac con3 2 1) (fac con3 6 4))
+             (fac con1
+                  (fac con2
+                       (fac con3 '(1 2) '(1 1))
+                       (fac con3 '(2 2) '(2 1)))
+                  (fac con3 '(3 2) '(4 1))))))
 
 
   #;
